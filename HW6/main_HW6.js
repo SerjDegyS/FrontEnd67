906e02bb1be4c0309c2920635624687be09c2874
SerjDegys <degyse@gmail.com>
2018-11-10 19:23:59 +0200

    finish HW6

--- HW6/main_HW6.js
+++ HW6/main_HW6.js
@@ -2,15 +2,19 @@
 (function () {
     var elements = document.querySelector("#box"),
         inputSize = document.querySelector('#size-circle')
-    console.dir(inputSize);
-
+    
 
     elements.style.border = "5px solid red";
-    console.log(document.body.style.margin);
+    console.log(elements.style.width);
 
 
     elements.addEventListener('click', function (e) {
-        
+        elementStyle = window.getComputedStyle(elements);
+
+        if (inputSize.value > elementStyle.width.slice(0, -2)) {
+            alert("Size of circle more then size of box!");
+            return;
+        }
         if (e.target.id != 'box') return;
 
         var circleClassName = 'circle',
@@ -18,16 +22,12 @@
             x = e.clientX,
             y = e.clientY;    
         circle.className = circleClassName;
-        console.log(inputSize.value) ;
+
         circle.style.width = inputSize.value + 'px';
         circle.style.height = inputSize.value + 'px';
         
-
-        
         circle.style.left = x - 13 - (inputSize.value/2) + 'px';
         circle.style.top = y - 13 - (inputSize.value/2) + 'px';
-        // document.body.style.margin = '0px';
-        console.log(document.body.style.margin);
         
         e.target.appendChild(circle);
 
